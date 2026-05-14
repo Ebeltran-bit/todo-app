4c08752acfc48585fb576237e8fa33bbbf02b72a
Ebeltran-bit <ebelrod04@gmail.com>
2026-05-14 09:35:06 +0200

    add paths at route.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login/login-page.component';
+import { SigninPageComponent } from './pages/signin/signin-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: LoginPageComponent
+
+    },
+    
+    {
+        path: 'signin',
+        component: SigninPageComponent
+    }
+];
