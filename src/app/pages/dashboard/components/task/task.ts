25a327892b9c2f5eaecef29096a06eb5986c19fc
Ebeltran-bit <ebelrod04@gmail.com>
2026-05-21 12:35:34 +0200

    feat: right sidebar for new tasks interface

--- src/app/pages/dashboard/components/task/task.ts
+++ src/app/pages/dashboard/components/task/task.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
+import { DatePickerModule } from 'primeng/datepicker';
+import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive,  FormsModule, DatePickerModule, SelectModule],
   templateUrl: './task.html',
   styleUrls: ['../../styles.css']
 })
 
-export class TaskComponent {}
+export class TaskComponent {
+  categories = [
+    { name: 'Category 1' },
+    { name: 'Category 2' }
+  ];
+  status = [
+    { name: 'Non started' },
+    { name: 'In progress' },
+    { name: 'Paused' },
+    { name: 'Late' },
+    { name: 'Finished' }
+  ];
+  selectedCategory: any = null;
+  selectedStatus: any = null;
+  dueDate: Date | null = null;
+}
