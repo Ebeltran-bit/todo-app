8ad5e86af52fb83741ad3e3550026e673c24c799
Ebeltran-bit <ebelrod04@gmail.com>
2026-05-22 09:26:25 +0200

    feat: nonStarted-page archives creation

---
+++ src/app/pages/nonstarted-page/nonstarted-page.ts
@@ -0,0 +1,8 @@
+import {Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nonstarted-page',
+  imports: [],
+  templateUrl: './nonstarted-page.html',
+})
+export default class NonstartedPageComponent {}
