53f24cf89ebd8808bfa92d3d35ad3fc8daacc3b9
Ebeltran-bit <ebelrod04@gmail.com>
2026-05-28 11:29:24 +0200

    feat: nonStarted page created tasks layout and new component creation

--- src/app/pages/dashboard/components/nonstarted-page/nonstarted-page.ts
+++ src/app/pages/dashboard/components/nonstarted-page/nonstarted-page.ts
@@ -1,9 +1,42 @@
-import {Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AccordionModule } from 'primeng/accordion';
 import { TaskViewComponent } from "../../../../shared/components/task-view.component/task-view.component";
+
+interface NonstartedTasks {
+  task: string;
+  date: Date;
+  subtask: string;
+  category: string;
+}
 
 @Component({
   selector: 'app-nonstarted-page',
-  imports: [TaskViewComponent],
+  standalone: true,
+  imports: [TaskViewComponent, CommonModule, AccordionModule],
   templateUrl: './nonstarted-page.html',
 })
-export default class NonstartedPageComponent {}
+export default class NonstartedPageComponent {
+  nonstartedTasks: NonstartedTasks[] = [
+    { 
+      task: "Task 1", 
+      date: new Date("2026-06-01"), 
+      subtask: "Subtask 1", 
+      category: "Category 1" 
+    },
+
+    { 
+      task: "Task 2", 
+      date: new Date("2026-06-04"), 
+      subtask: "Subtask 2", 
+      category: "Category 2" 
+    },
+
+    { 
+      task: "Task 3", 
+      date: new Date("2026-06-10"), 
+      subtask: "Subtask 3", 
+      category: "Category 1" 
+    }
+  ];
+}
---
+++ src/app/shared/components/tasks-accordion.component/tasks-accordion.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-acordion.component',
+  imports: [],
+  templateUrl: './tasks-acordion.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TasksAcordionComponent {}
