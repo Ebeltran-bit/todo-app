10896109c9889e88233af08a9a091f77b390a9b7
Ebeltran-bit <ebelrod04@gmail.com>
2026-05-28 13:26:15 +0200

    feat: finished tasks option accordion component implementation

--- src/app/pages/dashboard/components/finished-page/finished-page.ts
+++ src/app/pages/dashboard/components/finished-page/finished-page.ts
@@ -1,10 +1,36 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TaskViewComponent } from "../../../../shared/components/task-view.component/task-view.component";
+import { Task, TasksAccordionComponent } from '../../../../shared/components/tasks-accordion.component/tasks-accordion.component';
 
 @Component({
   selector: 'app-finished-page',
-  imports: [TaskViewComponent],
+  imports: [TaskViewComponent, TasksAccordionComponent],
   templateUrl: './finished-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class FinishedPageComponent {}
+export default class FinishedPageComponent {
+
+  finishedTasks: Task[] = [
+        { 
+          task: "Task 1", 
+          date: new Date("2026-06-01"), 
+          subtask: "Subtask 1", 
+          category: "Category 1" 
+        },
+    
+        { 
+          task: "Task 2", 
+          date: new Date("2026-06-04"), 
+          subtask: "Subtask 2", 
+          category: "Category 2" 
+        },
+    
+        { 
+          task: "Task 3", 
+          date: new Date("2026-06-10"), 
+          subtask: "Subtask 3", 
+          category: "Category 1" 
+        }
+      ];
+
+}
