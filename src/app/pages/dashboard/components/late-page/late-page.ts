be2c71cab4f11da608e0c8e0ea795c482725abe2
Ebeltran-bit <ebelrod04@gmail.com>
2026-05-28 13:23:40 +0200

    feat: late tasks option accordion component implementation

--- src/app/pages/dashboard/components/late-page/late-page.ts
+++ src/app/pages/dashboard/components/late-page/late-page.ts
@@ -1,10 +1,23 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TaskViewComponent } from "../../../../shared/components/task-view.component/task-view.component";
+import { Task, TasksAccordionComponent } from '../../../../shared/components/tasks-accordion.component/tasks-accordion.component';
 
 @Component({
   selector: 'app-late-page',
-  imports: [TaskViewComponent],
+  imports: [TaskViewComponent, TasksAccordionComponent],
   templateUrl: './late-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class LatePageComponent {}
+export default class LatePageComponent {
+
+  lateTasks: Task[] = [
+      { 
+        task: "Task 1", 
+        date: new Date("2026-06-01"), 
+        subtask: "Subtask 1", 
+        category: "Category 1" 
+      }
+  
+    ];
+
+}
