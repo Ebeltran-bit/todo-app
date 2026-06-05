fb8c18e5d068e2885c3d0f0680bc549dde140436
Ebeltran-bit <ebelrod04@gmail.com>
2026-06-05 07:24:12 +0200

    feat: new category page code

---
+++ src/app/pages/dashboard/components/category-page/category-page.ts
@@ -0,0 +1,28 @@
+import {Component } from '@angular/core';
+import { TaskViewComponent } from "../../../../shared/components/task-view.component/task-view.component";
+import { Task, TasksAccordionComponent } from '../../../../shared/components/tasks-accordion.component/tasks-accordion.component';
+
+
+@Component({
+  selector: 'app-category-page',
+  imports: [TaskViewComponent, TasksAccordionComponent],
+  templateUrl: './category-page.html',
+})
+export class CategoryPageComponent {
+
+  categoryTasks: Task[] = [
+        { 
+          task: "Task 1", 
+          date: new Date("2026-06-01"), 
+          subtask: "Subtask 1", 
+          category: "Category 1" 
+        },
+    
+        { 
+          task: "Task 2", 
+          date: new Date("2026-06-04"), 
+          subtask: "Subtask 2", 
+          category: "Category 2" 
+        }
+      ];
+}
